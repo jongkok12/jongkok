d28d0590784e7e02cd49af382e0804b68e0eed3a
jongkok12 <118080976+jongkok12@users.noreply.github.com>
2023-04-20 01:56:44 +0700

    inidian

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 
+const express = require('express');
+const app = express();
+const port = 3000;
 
+const jsonData = require('./data.json');
+
+app.get('/data', (req, res) => {
+  res.json(jsonData);
+});
+
+app.listen(port, () => {
+  console.log(`Aplikasi berjalan di http://localhost:${port}/data`);
+});
 
 const axios = require('axios');
 const cheerio = require('cheerio');
